c2f1722cd1a8b9cf5ea4e6912fbe5e3fc8eaa798
Michele Volpato <mvolpato@users.noreply.github.com>
2021-01-03 16:27:47 +0100

    Day 50

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,99 +2,51 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by Martin on 03/01/2021.
+//  Created by Michele Volpato on 03/01/2021.
 //
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    @Published var name = "Paul Hudson"
-    
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
+    @ObservedObject var order = Order()
     
     var body: some View {
-            VStack {
-                List(results, id: \.trackId) { item in
-                    VStack(alignment: .leading) {
-                        Text(item.trackName)
-                            .font(.headline)
-                        Text(item.collectionName)
-                    }
-                }
-                Form {
-                    Section {
-                        TextField("Username", text: $username)
-                        TextField("Email", text: $email)
-                    }
-                    Section {
-                        Button("Create account") {
-                            print("Creating account…")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
                         }
                     }
-                    .disabled(disableForm)
-                }
-            }
-            .onAppear(perform: loadData)
-    }
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
+
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
                     }
-                    
-                    // everything is good, so we can exit
-                    return
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
                 }
             }
-            
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
-        
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
