e451c1672d33ef747631986f925eb946bc2e8b95
Michele Volpato <mvolpato@users.noreply.github.com>
2021-01-03 15:20:21 +0100

    Day 49

---
+++ Shared/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  Shared
+//
+//  Created by Martin on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
